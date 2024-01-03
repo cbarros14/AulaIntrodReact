4cd96a78f49a1e35a19273a75892338e9d5f6ad1
Chris Barros <cbarrosbio@gmail.com.br>
2024-01-03 14:51:42 -0300

    myapp

---
+++ my-app/src/reverse/Reverse.js
@@ -0,0 +1,11 @@
+import './reverse.css'
+const Reverse = {(children)} => {
+    const reversetext = children.split('').Reverse().join('');
+    console.log(reversetext);
+
+    return 
+        <p> {'reversetext'}</P>
+        
+    
+
+export default reverse;
